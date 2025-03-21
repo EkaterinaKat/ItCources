f65909743c083aea63e62486321e6a5b161ac927
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2025-03-21 18:16:45 +0500

    bottom navigation menu

--- app/src/main/java/com/katyshevtseva/itcourses/MainActivity.kt
+++ app/src/main/java/com/katyshevtseva/itcourses/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.katyshevtseva.itcourses
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.katyshevtseva.itcourses.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            val selectedFragment: Fragment = when (item.itemId) {
+                R.id.navigation_home -> HomeFragment()
+                R.id.navigation_favorites -> FavoritesFragment()
+                R.id.navigation_account -> AccountFragment()
+                else -> throw RuntimeException("Unknown item id")
+            }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, selectedFragment)
+                .commit()
+
+            true
+        }
     }
 }
